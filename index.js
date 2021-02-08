be3b231b9b9b7f5aeef37093ad913a87e5dac59a
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-02-08 18:32:47 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var name = $('input[name="name"]').val();
-        var zip11 = $('input[name="zip11"]').val();
+        var zip11 = $('textarea[name="zip11"]').val();
         var addr11 = $('textarea[name="addr11"]').val();
         var tel = $('textarea[name="tel"]').val();
         var date = $('textarea[name="date"]').val();
@@ -14,19 +14,9 @@ $(function () {
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg1 = `${key}\n名前：${name}`;
-        var msg2 = `${key}\n郵便番号：${zip11}}`;
-        var msg3 = `${key}\n住所：${addr11}}`;
-        var msg4 = `${key}\nTEL：${tel}`;
-        var msg5 = `${key}\n日付：${date}`;
-        var msg6 = `${key}\n内容：date${reasons}`;
-        sendText(msg1);
-        sendText(msg2);
-        sendText(msg3);
-        sendText(msg4);
-        sendText(msg5);
-        sendText(msg6);
-        
+        var msg = `${key}\nお名前：${name}\n郵便番号：${zip11}\nご住所：${addr11}\nご連絡先：${tel}\n日付：${date}\n内容：${reasons}`;
+        sendText(msg);
+
         return false;
     });
 });
