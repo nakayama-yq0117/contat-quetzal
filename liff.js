6a94648e62f2c84eb4a8a7e786b89a6fbbefc669
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-02-09 00:10:46 +0900

    Add files via upload

--- liff.js
+++ liff.js
@@ -19,21 +19,21 @@ function initializeLiff(liffId) {
 }
 
 // function sendText(text) {
-//     // sendMessages(text);
-// // }
-
-// // LINEトーク画面上でメッセージ送信
-// // function sendMessages(text) {
-//     liff.sendMessages([{
-//         'type': 'text',
-//         'text': text
-//     },{
-//         "type": "sticker",
-//         "packageId": "11539",
-//         "stickerId": "52114113"
-//     }]).then(function () {
-//         liff.closeWindow();
-//     }).catch(function (error) {
-//         window.alert('Failed to send message ' + error);
-//     });
+//     sendMessages(text);
 // }
+
+// LINEトーク画面上でメッセージ送信
+function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    },{
+        "type": "sticker",
+        "packageId": "11539",
+        "stickerId": "52114113"
+    }]).then(function () {
+        liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
