f9454696767470caeb7ac5b927757e6960621c0e
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-02-08 18:10:14 +0900

    Add files via upload

--- liff.js
+++ liff.js
@@ -25,12 +25,8 @@ function sendText(text) {
 // LINEトーク画面上でメッセージ送信
 // function sendMessages(text) {
     liff.sendMessages([{
-        // 'type': 'text',
+        'type': 'text',
         'text': text
-    },{
-        "type": "sticker",
-        "packageId": "11539",
-        "stickerId": "52114113"
     }]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {
